4caa695a2b1f5d9261f1ddfe3b08a64f14ea9acb
Phantisy <phantisy@gmail.com>
2020-11-13 09:41:12 -0500

    Add files via upload

--- Supreme-monitor.py
+++ Supreme-monitor.py
@@ -1,43 +1,44 @@
 import requests
+import time
+from os import system
 from bs4 import BeautifulSoup
 from dhooks import Webhook, Embed
-import time
 from fake_headers import Headers
+system('clear')
 headers = Headers(os="mac", headers=True).generate() #os is the operating system you want the header to be
-url = 'https://www.supremenewyork.com/shop/accessories/g7u54g3ja/bxd1pzhyu'
+#url = 'https://www.supremenewyork.com/shop/accessories/g7u54g3ja/bxd1pzhyu'
 Delay = 5 #Delay between each requests that you send to monitor the site
-response = requests.get(url, headers=headers).text
-soup = BeautifulSoup(response, 'lxml') 
-x = True
-while x:
-    try:
-        if soup.find('input',{'class':'button'}):
-            print("in stock")
-            price_x = soup.find('p', class_='price').text
-            name_x = soup.find('h2', class_='protect').text
-            picture = soup.find(id='img-main')
 
-            film_id = 'img-main'
-            raw_picture = soup.find(itemprop="image")
-            picture_x = 'https:' + (raw_picture["src"])
-            print(picture_x)
-            x = False
-            embed = Embed(title='Supreme Restock', description='', color=0x5CDBF0, timestamp='now')
-            hook = Webhook("https://discord.com/api/webhooks/775197355234426900/nbFhxu4gWP8dPrenMIf_xzSBxiHHkaMKGn6Tx3KTTHiLmQH-XWkBGtG2hugrxxScLKjs")
-            embed.set_author(name='Supreme monitor')
-            embed.set_image(picture_x)
-            embed.add_field(name="Product", value=name_x)
-            embed.add_field(name="Price", value=price_x)
-            embed.add_field(name="Link", value=url)
-            embed.set_footer(text='This bot was coded by Venus#9210')
-            hook.send(embed=embed)
+def spmonitor():
+    url = input("Enter link to monitor: ")
+    wbhook = input("Enter webhook channel:" )
+    response = requests.get(url, headers=headers).text
+    soup = BeautifulSoup(response, 'lxml')
+    x = True
+    while x:
+        try:
+            if soup.find('input',{'class':'button'}):
+                print("in stock")
+                price_x = soup.find('p', class_='price').text
+                name_x = soup.find('h2', class_='protect').text
+                picture = soup.find(id='img-main')
+
+                film_id = 'img-main'
+                raw_picture = soup.find(itemprop="image")
+                picture_x = 'https:' + (raw_picture["src"])
+                print(picture_x)
+                x = False
+                embed = Embed(title='Supreme Restock', description='', color=0x5CDBF0, timestamp='now')
+                hook = Webhook(wbhook)
+                embed.set_author(name='Supreme Monitor')
+                embed.set_image(picture_x)
+                embed.add_field(name="Product", value=name_x)
+                embed.add_field(name="Price", value=price_x)
+                embed.add_field(name="Link", value=url)
+                embed.set_footer(text='This bot was coded by Venus#9210')
+                hook.send(embed=embed)
+                time.sleep(Delay)
+        except:
+            print("Error finding product or out of stock.")
             time.sleep(Delay)
-
-    except:
-        print("error finding product or out of stock")
-        time.sleep(Delay)
-        x = False
-       
-
-
-    
+            x = False
